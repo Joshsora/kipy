19d2d9ad56a707959f0b7579c94ddcd57f64895f
pythonology <pythonology@gmail.com>
2018-04-02 01:19:51 -0400

    Tabs -> Spaces

--- src/dml.cpp
+++ src/dml.cpp
@@ -7,16 +7,16 @@
     py::class_<Field<TYPE>>(m, NAME)                                            \
         .def(py::init<std::string>())                                           \
         .def_property_readonly("name", &Field<TYPE>::get_name,                  \
-			py::return_value_policy::copy)                                      \
-		.def_property_readonly("transferable", &Field<TYPE>::is_transferable,   \
-			py::return_value_policy::copy)                                      \
-		.def_property("value", &Field<TYPE>::get_value,                         \
-			static_cast<void (Field<TYPE>::*)(TYPE)>(&Field<TYPE>::set_value),  \
+            py::return_value_policy::copy)                                      \
+        .def_property_readonly("transferable", &Field<TYPE>::is_transferable,   \
+            py::return_value_policy::copy)                                      \
+        .def_property("value", &Field<TYPE>::get_value,                         \
+            static_cast<void (Field<TYPE>::*)(TYPE)>(&Field<TYPE>::set_value),  \
             py::return_value_policy::copy)                                      \
         .def_property_readonly("type_name", &Field<TYPE>::get_type_name,        \
-			py::return_value_policy::copy)                                      \
+            py::return_value_policy::copy)                                      \
         .def_property_readonly("size", &Field<TYPE>::get_size,                  \
-			py::return_value_policy::copy)
+            py::return_value_policy::copy)
 
 #define DEF_RECORD_FIELD_METHODS(PREFIX)                               \
     .def(#PREFIX ## "_byt_field", &Record::PREFIX ## _field<BYT>)      \
@@ -36,27 +36,27 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(dml, m)
 {
-	// Record
+    // Record
     py::class_<Record>(m, "Record")
         .def(py::init<>())
-		DEF_RECORD_FIELD_METHODS(has)
-		DEF_RECORD_FIELD_METHODS(add)
-		DEF_RECORD_FIELD_METHODS(get)
-		.def_property_readonly("field_count", &Record::get_field_count,
-			py::return_value_policy::copy)
+        DEF_RECORD_FIELD_METHODS(has)
+        DEF_RECORD_FIELD_METHODS(add)
+        DEF_RECORD_FIELD_METHODS(get)
+        .def_property_readonly("field_count", &Record::get_field_count,
+            py::return_value_policy::copy)
         .def_property_readonly("size", &Record::get_size,
-			py::return_value_policy::copy);
+            py::return_value_policy::copy);
 
     // Fields
-	DEF_FIELD_CLASS(BYT, "BytField");
-	DEF_FIELD_CLASS(UBYT, "UBytField");
-	DEF_FIELD_CLASS(SHRT, "ShrtField");
-	DEF_FIELD_CLASS(USHRT, "UShrtField");
-	DEF_FIELD_CLASS(INT, "IntField");
-	DEF_FIELD_CLASS(UINT, "UIntField");
-	DEF_FIELD_CLASS(STR, "StrField");
-	DEF_FIELD_CLASS(WSTR, "WStrField");
-	DEF_FIELD_CLASS(FLT, "FltField");
-	DEF_FIELD_CLASS(DBL, "DblField");
-	DEF_FIELD_CLASS(GID, "GidField");
+    DEF_FIELD_CLASS(BYT, "BytField");
+    DEF_FIELD_CLASS(UBYT, "UBytField");
+    DEF_FIELD_CLASS(SHRT, "ShrtField");
+    DEF_FIELD_CLASS(USHRT, "UShrtField");
+    DEF_FIELD_CLASS(INT, "IntField");
+    DEF_FIELD_CLASS(UINT, "UIntField");
+    DEF_FIELD_CLASS(STR, "StrField");
+    DEF_FIELD_CLASS(WSTR, "WStrField");
+    DEF_FIELD_CLASS(FLT, "FltField");
+    DEF_FIELD_CLASS(DBL, "DblField");
+    DEF_FIELD_CLASS(GID, "GidField");
 }
