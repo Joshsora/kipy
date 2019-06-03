7ca503df06e05ab3ff7517e3b7c2f4435949dfae
pythonology <pythonology@gmail.com>
2019-06-02 22:48:42 -0400

    Support BinarySerializer subclassing in Python

--- src/serialization.cpp
+++ src/serialization.cpp
@@ -4,6 +4,7 @@
 #include <ki/serialization/JsonSerializer.h>
 #include <ki/pclass/Type.h>
 #include <ki/pclass/TypeSystem.h>
+#include <ki/pclass/PropertyClass.h>
 
 namespace py = pybind11;
 
@@ -17,11 +18,36 @@ using ki::pclass::PropertyClass;
 using ki::serialization::BinarySerializer;
 using ki::serialization::JsonSerializer;
 
+namespace
+{
+    /**
+    * A Python alias class for BinarySerializer.
+    */
+    class PyBinarySerializer : public BinarySerializer
+    {
+    public:
+        using BinarySerializer::BinarySerializer;
+
+        bool presave_object(const PropertyClass *object, BitStream &stream) const override
+        {
+            PYBIND11_OVERLOAD(bool, BinarySerializer, presave_object, object, stream);
+        }
+    };
+
+    /**
+    * Allows for public access to the BinarySerializer::presave_object method.
+    */
+    class BinarySerializerPublicist : public BinarySerializer
+    {
+    public:
+        using BinarySerializer::presave_object;
+    };
+}
+
 void bind_serialization(py::module &m)
 {
-
     // Classes
-    py::class_<BinarySerializer> binary_serializer_cls(m, "BinarySerializer");
+    py::class_<BinarySerializer, PyBinarySerializer> binary_serializer_cls(m, "BinarySerializer");
     py::class_<JsonSerializer> json_serializer_cls(m, "JsonSerializer");
 
     py::enum_<BinarySerializer::flags>(m, "BinarySerializerFlags", py::arithmetic())
@@ -32,6 +58,7 @@ void bind_serialization(py::module &m)
     // BinarySerializer Definitions
     binary_serializer_cls.def(py::init<const TypeSystem &, bool, BinarySerializer::flags>(),
         "type_system"_a, "is_file"_a, "flags"_a);
+    binary_serializer_cls.def("presave_object", &BinarySerializerPublicist::presave_object);
     binary_serializer_cls.def("save", &BinarySerializer::save, "object"_a, "stream"_a);
     binary_serializer_cls.def("load", [](BinarySerializer &self, BitStream &stream, std::size_t size = 0)
     {
